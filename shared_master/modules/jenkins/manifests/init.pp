4298db34f7d8920298f41b842bc05f6304a2e1aa
JosephHardy <joseph.hardy@qa.com>
2016-12-19 09:02:51 -0600

    Updated Java and Jenkins modules
    "Updated Java and Jenkins modules"

---
+++ shared_master/modules/jenkins/manifests/init.pp
@@ -0,0 +1,37 @@
+class jenkins ()
+
+{
+        require java
+
+        Exec {
+                path => ["/usr/bin","/bin","/usr/sbin"]
+        }
+
+        exec { 'copy jenkins' :
+                command => 'sudo cp /tmp/shared/jenkins_2.1_all.deb /opt',
+        }
+
+        exec { 'add key' :
+                cwd => '/opt',
+                command => 'wget -q -O - https://pkg.jenkins.io/debian/jenkins.$
+        }
+
+        exec { 'update source list' :
+                command => 'sh -c "echo deb /opt/jenkins_2.1_all.deb > /etc/apt$
+        }
+
+        package { 'jenkins':
+                provider => dpkg,
+                ensure => installed,
+                source => "/opt/jenkins_2.1_all.deb",
+                require => Exec['update source list']
+        }
+
+        service { 'jenkins':
+                enable => true,
+                ensure => running,
+                hasrestart => true,
+                hasstatus => true,
+                require => Package['jenkins']
+        }
+}
