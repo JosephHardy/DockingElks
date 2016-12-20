110bafd37fcadcce401d1ba8158d7d47cf0f589b
Daniel Stevenson <dcas1995@gmail.com>
2016-12-20 14:43:47 +0000

    bootstrap_master updated

--- bootstrap_master.sh
+++ bootstrap_master.sh
@@ -30,16 +30,16 @@ sudo cp -r /tmp/shared/modules/snort /etc/puppet/modules
 sudo cp -r /tmp/shared/modules/tomcat /etc/puppet/modules
 
 sudo mkdir /opt/packer
-sudo mkdir /opt/binaries
 
-sudo cp -r /tmp/shared/binaries/elk-binary /opt/binaries
-sudo cp -r /tmp/shared/binaries/haproxy-binary /opt/binaries
-sudo cp -r /tmp/shared/binaries/java-binary /opt/binaries
-sudo cp -r /tmp/shared/binaries/jenkins-binary /opt/binaries
-sudo cp -r /tmp/shared/binaries/jira-binary /opt/binaries
-sudo cp -r /tmp/shared/binaries/maven-binary /opt/binaries
-sudo cp -r /tmp/shared/binaries/nexus-binary /opt/binaries
-sudo cp -r /tmp/shared/binaries/packer-binary /opt/binaries
-sudo cp -r /tmp/shared/binaries/snort-binary /opt/binaries
-sudo cp -r /tmp/shared/binaries/tomcat-binary /opt/binaries
-
+sudo cp /tmp/shared/binaries/elasticsearch-5.1.1.deb /etc/puppet/modules/elk/files
+sudo cp /tmp/shared/binaries/kibana-5.1.1-linux-x86_64.tar.gz /etc/puppet/modules/elk/files
+sudo cp /tmp/shared/binaries/logstash-5.1.1.tar.gz /etc/puppet/modules/elk/files
+sudo cp /tmp/shared/binaries/haproxy-1.7.1.tar.gz/etc/puppet/modules/haproxy/files
+sudo cp /tmp/shared/binaries/java.tar.gz /etc/puppet/modules/java/files
+sudo cp /tmp/shared/binaries/jenkins_2.1_all.deb /etc/puppet/modules/jenkins/files
+sudo cp /tmp/shared/binaries/jira.bin /etc/puppet/modules/jira/files
+sudo cp /tmp/shared/binaries/maven.tar.gz /etc/puppet/modules/maven/files
+sudo cp /tmp/shared/binaries/nexus-3.0.2-02-unix.tar.gz /etc/puppet/modules/nexus/files
+sudo cp /tmp/shared/binaries/packer_0.12.1_linux_amd64.zip /etc/puppet/modules/packer/files
+sudo cp /tmp/shared/binaries/daq-2.0.6.tar.gz /etc/puppet/modules/snort/files
+sudo cp /tmp/shared/binaries/snort-2.9.9.0.tar.gz /etc/puppet/modules/snort/files
