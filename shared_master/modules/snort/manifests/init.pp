9b0b7f9dac492cb55fbebb422af151b79bcaeb94
jcqac <johnny.che@qa.com>
2016-12-20 13:22:29 +0000

    Snort Module is now working.
    Snort module is now working, just need to make changes to the snort config file to tailor it to our respective networks and services.
    
    Signed-off-by: jcqac <johnny.che@qa.com>

--- shared_master/modules/snort/manifests/init.pp
+++ shared_master/modules/snort/manifests/init.pp
@@ -1,8 +1,10 @@
 #parameterised for future change/updates
 class snort($snort_archive	= 'snort-2.9.9.0.tar.gz',
 			$snort_folder	= 'snort-2.9.9.0',
+			$snort_deb		= 'snort_2.9.9.0-1_amd64.deb',
 			$snort_daq		= 'daq-2.0.6.tar.gz',
-			$snort_daq_fol	= 'daq-2.0.6'){
+			$snort_daq_fol	= 'daq-2.0.6',
+			$snort_daq_deb	= 'daq_2.0.6-1_amd64.deb'){
 
 	Exec {
 		path	=>	['/usr/bin', '/usr/sbin', '/bin'],
@@ -103,7 +105,7 @@ class snort($snort_archive	= 'snort-2.9.9.0.tar.gz',
 	exec { 'extract_snort' :
 		cwd	=> '/opt/snort/',
 		command	=> "sudo tar zxvf ${snort_archive}",
-		require	=> File["/opt/snort/${snort_snort_daq}"],
+		require	=> File["/opt/snort/${snort_daq}"],
 		before	=> Exec['extract_snort_daq'],
 	}
 
@@ -121,54 +123,61 @@ class snort($snort_archive	= 'snort-2.9.9.0.tar.gz',
 	exec { 'disable_eth_feature1' : 
 	command	=> "sudo bash -c \"echo 'post-up ethtool -K eth0 lro off' >> /etc/network/interfaces\"",
 	require	=> Exec['extract_snort_daq'],
+	before	=> Exec['disable_eth_feature2'],
 	}
 
 	exec { 'disable_eth_feature2' : 
 	command	=> "sudo bash -c \"echo 'post-up ethtool -K eth0 gro off' >> /etc/network/interfaces\"",
 	require	=> Exec['disable_eth_feature1'],
+	before	=> Exec['install_daq'],
 	}
 	#NETORK CONFIG END
 
 	#COMPILE SNORT DAQ AND INSTALL
 	exec { 'install_daq' : 
 		cwd		=> "/opt/snort/${snort_daq_fol}",
-		command	=> "sudo bash -c '/opt/snort/${snort_daq_fol}/configure && make && make install'",
+		command	=> "sudo bash -c '/opt/snort/${snort_daq_fol}/configure && make && sudo make install'",
 		require	=> Exec['disable_eth_feature2'],
-		#user 	=> "root",
+		before	=> Exec['install_snort'],
 	}
 
 	#COMPILE SNORT AND INSTALL
 	exec { 'install_snort' : 
 		cwd		=> "/opt/snort/${snort_folder}",
-		command	=> "sudo bash -c '/opt/snort/${snort_folder}/configure --enable-sourcefire && make && make install'",
+		command	=> "sudo bash -c '/opt/snort/${snort_folder}/configure --enable-sourcefire && make && sudo make install'",
 		require	=> Exec['install_daq'],
-		#user 	=> "root",
+		before	=> Exec['update_lib'],
 	}
+	
 	#INTALL END
 #INSTALL END END
 
 #CONFIGURING SNORT
 	#RUN COMMAND TO UPDATE SHARED LIBS
 	exec { 'update_lib' : 
-		command	=> 'sudo ldconfig',
+		command	=> 'sudo ldconfig -v',
 		require	=> Exec['install_snort'],
+		before	=> Exec['sym_snort'],
 	}
 
 	#CREATE SYMLINK FOR SNORT SERVICE
 	exec { 'sym_snort' : 
 		command	=> 'sudo ln -s /usr/local/bin/snort /usr/sbin/snort',
 		require	=> Exec['update_lib'],
+		before	=> Exec['grp_snort'],
 	}
 
 	# CREATE SNORT USER AND GROUP
 	exec { 'grp_snort' : 
 		command	=> 'sudo groupadd snort',
 		require	=> Exec['sym_snort'],
+		before	=> Exec['usr_snort'],
 	}
 
 	exec { 'usr_snort' : 
 		command	=> 'sudo useradd snort -r -s /sbin/nologin -c SNORT_IDS -g snort',
 		require	=> Exec['grp_snort'],
+		before	=> File['/opt/snort/mkdir.sh'],
 	}
 	# CREATE SNORT USER END
 
@@ -177,6 +186,7 @@ class snort($snort_archive	= 'snort-2.9.9.0.tar.gz',
 		ensure 	=> present,
 		source  => 'puppet:///modules/snort/mkdir.sh',
 		require	=> Exec['usr_snort'],
+		before	=> Exec['mkdir_perm'],
 	}
 
 	#update shell script permissions
@@ -184,12 +194,14 @@ class snort($snort_archive	= 'snort-2.9.9.0.tar.gz',
 		cwd		=> '/opt/snort',
 		command	=> 'sudo chmod a+x /opt/snort/mkdir.sh',
 		require	=> File['/opt/snort/mkdir.sh'],
+		before	=> Exec['snort_mkdir'],
 	}
 
 	exec { 'snort_mkdir' : 
 		cwd		=> '/opt/snort',
 		command	=> "sudo bash -c '/opt/snort/mkdir.sh'",
 		require	=> Exec['mkdir_perm'],
+		before	=> Exec['update_snort_download'],
 	}
 
 	#CREATE FOLDERS END
@@ -199,36 +211,78 @@ class snort($snort_archive	= 'snort-2.9.9.0.tar.gz',
 		cwd		=> "/opt/snort/${snort_folder}",
 		command	=> 'sudo wget https://www.snort.org/rules/community',
 		require	=> Exec['snort_mkdir'],
+		before	=> Exec['update_snort'],
 	}
 
 	exec { "update_snort" : 
 		cwd		=> "/opt/snort/${snort_folder}",
 		command	=> 'sudo tar -xvfz community.tar.gz -C /etc/snort/rules',
 		require	=> Exec['update_snort_download'],
+		before	=> Exec['copy_1'],
 	}
 	#UPDATE SNORT END
-
+	
+	#COPY RESPECTIE CONF FILES FOR /etc/snort
+	exec { 'copy_1' : 
+		cwd		=> "/opt/snort/${snort_folder}/etc/",
+		command	=> 'sudo cp *.conf* /etc/snort',
+		require	=> Exec['update_snort'],
+		before	=> Exec['copy_2'],
+	}
+	
+	exec { 'copy_2' : 
+		cwd		=> "/opt/snort/${snort_folder}/etc/",
+		command	=> 'sudo cp *.map /etc/snort',
+		require	=> Exec['copy_1'],
+		before	=> Exec['copy_3'],
+	}
+	
+	exec { 'copy_3' : 
+		cwd		=> "/opt/snort/${snort_folder}/etc/",
+		command	=> 'sudo cp *.dtd /etc/snort',
+		require	=> Exec['copy_2'],
+		before	=> Exec['copy_4'],
+	}
+	
+	exec { 'copy_4' : 
+		cwd		=> "/opt/snort/${snort_folder}/src/dynamic-preprocessors/build/usr/local/lib/snort_dynamicpreprocessor/",
+		command	=> 'sudo cp * /usr/local/lib/snort_dynamicpreprocessor/',
+		require	=> Exec['copy_3'],
+		before	=> Exec['rem_conf'],
+	}
+	
+	exec { 'rem_conf' : 
+		cwd		=> '/etc/snort/',
+		command	=> 'sudo rm -f snort.conf',
+		require	=> Exec['copy_4'],
+		before	=> File['/etc/snort/snort.conf'],
+	}
+	
 	#COPY CONFIG FILE FOR SNORT
-	file {'/etc/snort/snort.conf' : 
+	file { '/etc/snort/snort.conf' : 
 		ensure  => present,
 		source  => 'puppet:///modules/snort/snort.conf',
-		require => Exec['update_snort'],
+		require => Exec['rem_conf'],
+		before	=> Exec['update_perm1'],
 	}
 
 	#UPDATE PERMISSIONS FOR SNORT USER
 	exec { 'update_perm1' : 
 		command	=> 'sudo chown -R snort:snort /etc/snort',
 		require	=> File['/etc/snort/snort.conf'],
+		before	=> Exec['update_perm2'],
 	}
 
 	exec { "update_perm2" : 
 		command	=> "sudo chown -R snort:snort /var/log/snort",
 		require	=> Exec['update_perm1'],
+		before	=> Exec['update_perm3'],
 	}
 
 	exec { "update_perm3" : 
 		command	=> "sudo chown -R snort:snort /usr/local/lib/snort_dynamicrules",
 		require	=> Exec['update_perm2'],
+		before	=> File['/etc/init/snort.init'],
 	}
 	#UPDATE PERMISSIONS END	
 
@@ -238,6 +292,7 @@ class snort($snort_archive	= 'snort-2.9.9.0.tar.gz',
 		ensure  => present,
 		source  => 'puppet:///modules/snort/snort.init',
 		require	=> Exec['update_perm3'],
+		before	=> Exec['rename_config'],
 	}
 
 	#RENAME snort.init FILE
@@ -245,23 +300,19 @@ class snort($snort_archive	= 'snort-2.9.9.0.tar.gz',
 		cwd		=> '/etc/init',
 		command	=> 'sudo mv ./snort.init ./snort.conf',
 		require	=> File['/etc/init/snort.init'],
+		before	=> Exec['update_ex'],
 	}
 
 	#UPDATE snort.conf Permissions
 	exec { "update_ex" : 
 		command	=> 'sudo chmod a+x /etc/init/snort.conf',
 		require	=> Exec['rename_config'],
-	}
-
-	#CHECK snort.conf EXECUTABLE
-	exec { "check_ex" : 
-		command	=> 'initctl list | grep snort',
-		require	=> Exec['update_ex'],
+		before	=> Exec['restart_snort'],
 	}
 
 	#RESTART snort SERVICE
 	exec { 'restart_snort' : 
-		command	=> 'snort stop/waiting',
-		require	=> Exec['check_ex'],
+		command	=> 'sudo service snort start',
+		require	=> Exec['update_ex'],
 	}
 }
