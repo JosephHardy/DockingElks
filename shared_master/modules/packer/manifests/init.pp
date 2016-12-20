21a84337446e8efbbc6b12edf7aab6e7c39c26b7
jcqac <johnny.che@qa.com>
2016-12-20 15:57:32 +0000

    Packer Module is Fixed.
    Instead of using .bashrc, we are using /etc/environment instead
    
    Packer is now working.
    
    Signed-off-by: jcqac <johnny.che@qa.com>

--- shared_master/modules/packer/manifests/init.pp
+++ shared_master/modules/packer/manifests/init.pp
@@ -1,53 +1,77 @@
-class packer (
-		$packer_zipfile = "packer_0.12.1_linux_amd64.zip" )
-		{
+#parameterised for future change/updates
+class packer($packer_archive	= 'packer_0.12.1_linux_amd64.zip'){
 
-        Exec {
-                path => ["/usr/bin", "/bin/", "/usr/sbin", "/opt/packer"]
-        }
+	Exec {
+		path	=>	['/usr/bin', '/usr/sbin', '/bin'],
+		returns =>	[0, 2, 14, 100],
+	}
 
-        exec { 'copy zip file' :
-                cwd => '/opt/packer',
-                command => 'sudo cp -r /opt/binaries/packer-binary/${packer_zipfile} .',
-        }
+#INSTALL PACKER START
+	#INSTALL DEPENDENCIES
+	#ensure folder exits
+	file { '/opt/packer/' : 
+		ensure	=> directory,
+		before	=> Package['unzip']
+	}
+	
+	package { 'unzip' : 
+		ensure 	=> present,
+		require	=> File['/opt/packer/'],
+		before	=> File["/opt/packer/${packer_archive}"],
+	}
 
-        exec { 'extract zip file' :
-                cwd => '/opt/packer/packer-binary/',
-                command => 'sudo unzip /opt/packer/packer-binary/${packer_zipfile}',
-                require => Exec['copy zip file']
-        }
-		
-		exec { 'copy bash script':
-				command => 'sudo cp /tmp/shared/editbashrc.sh /opt/packer',
-				require => Exec['extract zip file']
-		}
+	#INSTALL DEPENDENCIES END
 
-        file { 'editbashrc.sh' :
-                ensure => 'file',
-                path => '/opt/packer/editbashrc.sh',
-                owner => 'root',
-                mode => '755'
-				require => Exec['copy bash script']
-        }
-		
-		exec { 'change permissions' :
-				cwd => '/opt/packer/',
-				command => 'sudo chmod u+x /opt/packer/editbashrc.sh',
-				require => File['editbashrc.sh']
-		}
+#FOLDER START	
+	# EXTRACT ARCHIVE
+	file { "/opt/packer/${packer_archive}" : 
+		ensure  => present,
+		source  => "puppet:///modules/packer/${packer_archive}",
+		require => Package['unzip'],
+		before	=> Exec['extract_packer'],
+	}
 
-        exec { 'run bash script' :
-				cwd => '/opt/packer',
-                user    => 'root',
-				command => './editbashrc.sh',
-                refreshonly => true,
-                require => Exec['change permissions']
-        }
+	exec { 'extract_packer' :
+		cwd	=> '/opt/packer/',
+		command	=> "sudo unzip ${packer_archive}",
+		require	=> File["/opt/packer/${packer_archive}"],
+		before	=> File['/opt/packer/setpath.sh'],
+	}
+	# EXTRACT END
+#FOLDER END
 
-        exec { 'reboot VM' :
-                command => 'sudo reboot',
-                require => Exec['run bash script']
-        }
+#INSTALL BEGIN
+	#Add path to /etc/environment
+	
+	#Load sed shell script to append PATH in /etc/environment
+	file { '/opt/packer/setpath.sh' : 
+		ensure 	=> present,
+		source  => 'puppet:///modules/packer/setpath.sh',
+		require	=> Exec['extract_packer'],
+		before	=> Exec['shell_perm'],
+	}
 
+	#update shell script permissions
+	exec { 'shell_perm' : 
+		cwd		=> '/opt/packer',
+		command	=> 'sudo chmod a+x /opt/packer/setpath.sh',
+		require	=> File['/opt/packer/setpath.sh'],
+		before	=> Exec['path_app'],
+	}
+
+	exec { 'path_app' : 
+		cwd		=> '/opt/packer',
+		command	=> "sudo bash -c '/opt/packer/setpath.sh'",
+		require	=> Exec['shell_perm'],
+		before	=> Exec['load_path'],
+	}
+	
+	#load the new variables so the path can be read without reset.
+	exec { 'load_path' : 
+		command => "sudo bash -c \"source /etc/environment\"",
+		require	=> Exec['path_app'],
+	}
+	
+	#INTALL END
+#INSTALL END END
 }
-
