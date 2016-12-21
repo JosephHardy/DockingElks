a0ff03714d25fc49ac1c4171d2079c1e1737b9b5
JosephHardy <joseph.hardy@qa.com>
2016-12-21 08:44:41 -0600

    Fixed manifest and config filename

--- shared_master/modules/haproxy/manifests/init.pp
+++ shared_master/modules/haproxy/manifests/init.pp
@@ -9,25 +9,33 @@ class haproxy {
 
 	exec { 'install libssl' :
                 command => 'sudo apt-get install -y libssl-dev',
+		require => Exec['install build-essential'],
         }
 
 	exec { 'install libpcre' :
                 command => 'sudo apt-get install -y libpcre++-dev',
+		require => Exec['install libssl'],
         }
 
 	exec { 'create user' :
 		command => 'sudo useradd haproxy',
 	}
 
-	exec { 'copy haproxy' :
-		cwd => '/tmp/shared',
-		command => 'sudo cp /tmp/shared/haproxy-1.7.1.tar.gz /opt',
-	}
+#	exec { 'copy haproxy' :
+#		cwd => '/tmp/shared',
+#		command => 'sudo cp /tmp/shared/binaries/haproxy-1.7.1.tar.gz /opt',
+#	}
 
+	file { '/opt/haproxy-1.7.1.tar.gz' :
+                ensure => present,
+                source => 'puppet:///modules/haproxy/haproxy-1.7.1.tar.gz',
+		require => Exec['install libpcre'],
+        }
+	
 	exec { 'extract haproxy' :
                 cwd => '/opt',
-                command => 'sudo zxvf haproxy-1.7.1.tar.gz',
-		require => Exec['extract haproxy'],
+                command => 'sudo tar zxvf haproxy-1.7.1.tar.gz',
+		require => File['/opt/haproxy-1.7.1.tar.gz'],
         }
 
 	exec { 'compile haproxy' :
@@ -48,13 +56,14 @@ class haproxy {
 		require => Exec['install haproxy'],
 	}
 
-	exec { 'make directory' :
-		command => 'sudo mkdir /etc/haproxy',
+	file{ '/etc/haproxy' :
+		ensure  =>  directory,
+		mode    =>  0755,
 	}
 
 	file { '/etc/haproxy/haproxy.cfg' :
                 ensure => present,
                 source => 'puppet:///modules/haproxy/haproxy.cfg',
-                require => Exec['make directory'],
+                require => File['/etc/haproxy'],
         }
 }
