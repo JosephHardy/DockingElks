50e9d19e19a216ff700848a813778193e1f1fa2c
DCAS1995 <dcas1995@gmail.com>
2017-01-05 15:32:58 +0000

    Update filebeat.sh

--- shared_agent/filebeat.sh
+++ shared_agent/filebeat.sh
@@ -5,4 +5,4 @@ sudo tar -zxvf filebeat-5.1.1-linux-x86_64.tar.gz
 sudo cp /tmp/shared/filebeat.yml filebeat-5.1.1-linux-x86_64/
 cd filebeat-5.1.1-linux-x86_64/
 
-sudo ./filebeat -e -c filebeat.yml -d "publish" &
+sudo ./filebeat -e -c filebeat.yml -d "publish"
